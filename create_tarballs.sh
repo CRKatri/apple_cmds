cd21aeee23e543fde02c19655d4e631cd5118a41
Cameron Katri <me@cameronkatri.com>
2021-05-13 16:46:12 -0400

    create_tarballs.sh: Add script

---
+++ create_tarballs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+num=0
+for i in adv_cmds basic_cmds bootstrap_cmds \
+		developer_cmds diskdev_cmds doc_cmds \
+		file_cmds mail_cmds misc_cmds network_cmds \
+		patch_cmds remote_cmds shell_cmds \
+		system_cmds text_cmds; do
+	EXTRA_PATHS=""
+	case "$i" in
+		network_cmds) EXTRA_PATHS="lib/libpcap";;
+		remote_cmds) EXTRA_PATHS="lib/libtelnet";;
+	esac
+	num=$((num+1))
+	printf "(%i) %s\n" "$num" "$i"
+	tar caf $i.tar.zst $i ${EXTRA_PATHS}
+done
