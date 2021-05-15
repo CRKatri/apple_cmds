8987fe5b95d7e9bf1843aa5bf8be5065f27099fd
Cameron Katri <me@cameronkatri.com>
2021-05-14 22:57:02 -0400

    create_tarballs.sh: Track versions

--- create_tarballs.sh
+++ create_tarballs.sh
@@ -15,18 +15,15 @@ for i in adv_cmds basic_cmds bootstrap_cmds \
 		patch_cmds remote_cmds shell_cmds \
 		system_cmds text_cmds; do
 	EXTRA_PATHS=""
-	REGEX=""
+	VERSION="$(cat ${i}/.apple_version)"
+	REGEX="|^${i}|${i}-${VERSION}|"
 	case "$i" in
-		network_cmds) REGEX="|^|network_cmds/|" EXTRA_PATHS="lib/libpcap";;
-		remote_cmds) REGEX="|^|remote_cmds/|" EXTRA_PATHS="lib/libtelnet";;
+		network_cmds) REGEX="|^|network_cmds-${VERSION}/|" EXTRA_PATHS="lib/libpcap";;
+		remote_cmds) REGEX="|^|remote_cmds-${VERSION}/|" EXTRA_PATHS="lib/libtelnet";;
 	esac
 	num=$((num+1))
 	printf "(%i) %s\n" "$num" "$i"
 	cp setup.sh $i/setup.sh
-	if [ "${REGEX}" == "" ]; then
-		${TAR} -caf $i.tar.zst $i ${EXTRA_PATHS}
-	else
-		${TAR} -s ${REGEX} -caf $i.tar.zst $i ${EXTRA_PATHS}
-	fi
+	${TAR} -s ${REGEX} -caf ${i}-${VERSION}.tar.zst $i ${EXTRA_PATHS}
 	rm $i/setup.sh
 done
